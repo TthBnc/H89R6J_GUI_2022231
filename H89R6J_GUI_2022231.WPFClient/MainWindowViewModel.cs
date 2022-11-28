0b36a9e59e02170cc08755df93d339b47989b3a2
Bence Tóth <bence.zltn.toth@gmail.com>
2022-11-28 14:15:04 +0100

    MainWindowViewModel done for all 3 models

--- H89R6J_GUI_2022231.WPFClient/MainWindowViewModel.cs
+++ H89R6J_GUI_2022231.WPFClient/MainWindowViewModel.cs
@@ -14,7 +14,9 @@ namespace H89R6J_GUI_2022231.WPFClient
 {
     public class MainWindowViewModel : ObservableRecipient
     {
+        #region Characthers
         public RestCollection<Character> Characters { get; set; }
+
 
         public ICommand CreateCharacterCommand { get; set; }
         public ICommand DeleteCharacterCommand { get; set; }
@@ -43,7 +45,74 @@ namespace H89R6J_GUI_2022231.WPFClient
                 }
             }
         }
+        #endregion
 
+        #region Weapons
+        public RestCollection<Weapon> Weapons { get; set; }
+
+
+        public ICommand CreateWeaponCommand { get; set; }
+        public ICommand DeleteWeaponCommand { get; set; }
+        public ICommand UpdateWeaponCommand { get; set; }
+
+        private Weapon selectedWeapon;
+
+        public Weapon SelectedWeapon
+        {
+            get { return selectedWeapon; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedWeapon = new Weapon()
+                    {
+                        Id = value.Id,
+                        Name = value.Name,
+                        PeakDmg = value.PeakDmg,
+                        CharacterId = value.CharacterId
+
+                    };
+                    OnPropertyChanged();
+                    (DeleteWeaponCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Artifacts
+
+        public RestCollection<Artifact> Artifacts { get; set; }
+
+
+        public ICommand CreateArtifactCommand { get; set; }
+        public ICommand DeleteArtifactCommand { get; set; }
+        public ICommand UpdateArtifactCommand { get; set; }
+
+        private Artifact selectedArtifact;
+
+        public Artifact SelectedArtifact
+        {
+            get { return selectedArtifact; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedArtifact = new Artifact()
+                    {
+                        Id = value.Id,
+                        Name = value.Name,
+                        Cost = value.Cost,
+                        CharacterId = value.CharacterId
+                    };
+                    OnPropertyChanged();
+                    (DeleteArtifactCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+
+        #endregion
 
         public static bool IsInDesignMode
         {
@@ -58,9 +127,12 @@ namespace H89R6J_GUI_2022231.WPFClient
         {
             if (!IsInDesignMode)
             {
-                Characters = new RestCollection<Character>("http://localhost:8160/", "character");
+                Characters = new RestCollection<Character>("http://localhost:8160/", "character", "hub");
+                Weapons = new RestCollection<Weapon>("http://localhost:8160/", "weapon", "hub");
+                Artifacts = new RestCollection<Artifact>("http://localhost:8160/", "artifacts", "hub");
 
 
+                #region Character Commands
                 CreateCharacterCommand = new RelayCommand(
                     () => {
                         Characters.Add(new Character()
@@ -78,8 +150,54 @@ namespace H89R6J_GUI_2022231.WPFClient
 
                 UpdateCharacterCommand = new RelayCommand(
                     () => { Characters.Update(SelectedCharacter); });
+                #endregion
+
+                /////////////////////////////////////////////////////
+                
+                #region Weapon Commands
+                CreateWeaponCommand = new RelayCommand(
+                    () => {
+                        Weapons.Add(new Weapon()
+                        {
+                            Name = SelectedWeapon.Name,
+                            PeakDmg = SelectedWeapon.PeakDmg,
+                            CharacterId = SelectedWeapon.CharacterId
+                        });
+                    });
+
+                DeleteWeaponCommand = new RelayCommand(
+                    () => { Weapons.Delete(SelectedWeapon.Id); },
+                    () => { return SelectedWeapon != null; });
+
+                UpdateWeaponCommand = new RelayCommand(
+                    () => { Weapons.Update(SelectedWeapon); });
+
+                #endregion
+
+                /////////////////////////////////////////////////////
+
+                #region Artifact Commands
+                CreateArtifactCommand = new RelayCommand(
+                    () => {
+                        Artifacts.Add(new Artifact()
+                        {
+                            Name = SelectedArtifact.Name,
+                            Cost = SelectedArtifact.Cost,
+                            CharacterId = SelectedArtifact.CharacterId
+                        });
+                    });
+
+                DeleteArtifactCommand = new RelayCommand(
+                    () => { Artifacts.Delete(SelectedArtifact.Id); },
+                    () => { return SelectedArtifact != null; });
+
+                UpdateArtifactCommand = new RelayCommand(
+                    () => { Artifacts.Update(SelectedArtifact); });
+                #endregion
 
                 SelectedCharacter = new Character();
+                SelectedWeapon = new Weapon();
+                SelectedArtifact = new Artifact();
             }
         }
     }
