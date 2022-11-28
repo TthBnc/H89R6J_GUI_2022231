dfeb247590d44e46b4ddc528648d13437dfb835a
Bence Tóth <bence.zltn.toth@gmail.com>
2022-11-28 11:15:53 +0100

    Endpoints Character config (CController)

--- JEH01V_HFT_2021222.Endpoint/Controllers/CharacterController.cs
+++ JEH01V_HFT_2021222.Endpoint/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using JEH01V_HFT_2021222.Logic.Interfaces;
+﻿using JEH01V_HFT_2021222.Endpoint.Services;
+using JEH01V_HFT_2021222.Logic.Interfaces;
 using JEH01V_HFT_2021222.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
@@ -12,10 +13,12 @@ namespace JEH01V_HFT_2021222.Endpoint.Controllers
     public class CharacterController : ControllerBase
     {
         ICharacterLogic cLogic;
+        IHubContext<SignalRHub> hub;
 
-        public CharacterController(ICharacterLogic cLogic)
+        public CharacterController(ICharacterLogic cLogic, IHubContext<SignalRHub> hub)
         {
             this.cLogic = cLogic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -34,18 +37,22 @@ namespace JEH01V_HFT_2021222.Endpoint.Controllers
         public void Create([FromBody] Character value)
         {
             cLogic.Create(value);
+            this.hub.Clients.All.SendAsync("CharacterCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Character value)
         {
             cLogic.Update(value);
+            this.hub.Clients.All.SendAsync("CharacterUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var cToDelete = cLogic.Read(id);
             cLogic.Delete(id);
+            this.hub.Clients.All.SendAsync("CharacterDeleted", cToDelete);
         }
 
         [HttpGet("WeaponsOfGivenElement/{element}")]
